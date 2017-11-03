4e3637a630ac2e06b8b3aeb26104b7edb323b377
omnirepo <33350129+omnirepo@users.noreply.github.com>
2017-11-03 20:37:10 +0530

    msm8937

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 # TODO:  Find a better way to separate build configs for ADP vs non-ADP devices
 ifneq ($(TARGET_BOARD_AUTO),true)
-  ifneq ($(filter caf-msm8916 caf-msm8952 caf-msm8992 caf-msm8996 caf-msm8998,$(TARGET_QCOM_BLUETOOTH_VARIANT)),)
+  ifneq ($(filter caf-msm8916 caf-msm8952 caf-msm8992 caf-msm8937 caf-msm8998,$(TARGET_QCOM_BLUETOOTH_VARIANT)),)
     ifeq (caf-msm8994,$(TARGET_QCOM_BLUETOOTH_VARIANT))
         TARGET_QCOM_BLUETOOTH_VARIANT := caf-msm8992
     endif
@@ -12,8 +12,8 @@ ifneq ($(TARGET_BOARD_AUTO),true)
     ifneq ($(filter msm8994 msm8992,$(TARGET_BOARD_PLATFORM)),)
       include $(call all-named-subdir-makefiles,msm8992)
     else
-      ifneq ($(filter msm8996,$(TARGET_BOARD_PLATFORM)),)
-        include $(call all-named-subdir-makefiles,msm8996)
+      ifneq ($(filter msm8937,$(TARGET_BOARD_PLATFORM)),)
+        include $(call all-named-subdir-makefiles,msm8937)
       else
         ifneq ($(filter msm8909 ,$(TARGET_BOARD_PLATFORM)),)
           #For msm8909 target
